b7c5abb16b25ebfd35e6c1186b2b8ca9961c8dd4
DipikaRane <deepikajoge@gmail.com>
2022-06-08 17:42:09 +0530

    Bootstrap day 1

---
+++ CSS/reception.js
@@ -0,0 +1,8 @@
+function hidediv(){
+    var mydiv=document.getElementById('coupon');
+    mydiv.style.display="none";
+}
+function showdiv(){
+    var mydiv=document.getElementById('coupon');
+    mydiv.style.display="block";
+}
