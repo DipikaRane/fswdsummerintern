c66f5d78be500b09b804913124ef6723b2c17918
DipikaRane <deepikajoge@gmail.com>
2022-06-24 14:34:46 +0530

    MongodbLocal and Live url creation

--- restapi/apiDoc.md
+++ restapi/apiDoc.md
@@ -1,2 +1,27 @@
-localhost>>>>>
-location url - http://localhost:8425/location
+1. Home page (page 1)
+>>>features
+http://localhost:8425/features
+
+>>>location
+http://localhost:8425/location
+
+>>services
+http://localhost:8425/services
+
+
+Using params
+>>>location with location_id
+http://localhost:8425/location/location_id  
+http://localhost:8425/location/2
+
+>>>state with state_id
+http://localhost:8425/state/state_id
+http://localhost:8425/state/3
+
+>>>services with services_id
+http://localhost:8425/services/id
+http://localhost:8425/services/4
+
+>>>features with features_id
+http://localhost:8425/features/Feature_id
+http://localhost:8425/features/2
--- restapi/app.js
+++ restapi/app.js
@@ -2,8 +2,12 @@ var express=require('express');
 var port = process.env.PORT || 8425
 var app = express();
 var dotenv=require('dotenv');
+var mongo=require('mongodb');
+var MongoClient=mongo.MongoClient;
 dotenv.config();
+const mongoUrl=process.env.MongoLiveUrl;
 
+var db;
 
 //first default route
 app.get('/',(req,res)=>{
@@ -11,9 +15,71 @@ app.get('/',(req,res)=>{
 })
 
 app.get('/location',(req,res)=>{
-    res.send(location)
+    db.collection('location').find().toArray((err,result)=>{
+        if(err) throw err
+        res.send(result);
+    })
 })
 
-app.listen(port,()=>{
-    console.log(`listening on port ${port}`)
+app.get('/services',(req,res)=>{
+    db.collection('services').find().toArray((err,result)=>{
+        if(err) throw err
+        res.send(result)
+    })
+})
+
+app.get('/features',(req,res)=>{
+    db.collection('features').find().toArray((err,result)=>{
+        if(err) throw err
+        res.send(result)
+    })
+})
+
+app.get('/location/:id',(req,res)=>{
+    var id=parseInt(req.params.id);
+    // console.log(id);
+    // res.send('ok')
+    db.collection('location').find({"location_id":id}).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+app.get('/state/:id',(req,res)=>{
+    var id=parseInt(req.params.id);
+    // console.log(id);
+    // res.send('ok')
+    db.collection('location').find({"state_id":id}).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+app.get('/services/:id',(req,res)=>{
+    var id=parseInt(req.params.id);
+    // console.log(id);
+    // res.send('ok')
+    db.collection('services').find({"id":id}).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+app.get('/features/:id',(req,res)=>{
+    var id=parseInt(req.params.id);
+    // console.log(id);
+    // res.send('ok')
+    db.collection('features').find({"Feature_id":id}).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+//connect with mongodb 
+MongoClient.connect(mongoUrl,(err,client)=>{
+    if(err) console.log("Error while Connection");
+    db=client.db('HospitalData');
+    app.listen(port,()=>{
+        console.log(`listening on port ${port}`)
+    })
 })
